d3462ff105a94674e2556aa02aed19891b887794
JasonZ <jason.zeng@tinkerbox.com.sg>
2015-08-16 13:49:20 +0800

    Add pins routing tests

--- spec/routing/pins_routing_spec.rb
+++ spec/routing/pins_routing_spec.rb
@@ -2,38 +2,12 @@ require "rails_helper"
 
 RSpec.describe PinsController, type: :routing do
   describe "routing" do
-
-    it "routes to #index" do
-      expect(:get => "/pins").to route_to("pins#index")
-    end
-
-    it "routes to #new" do
-      expect(:get => "/pins/new").to route_to("pins#new")
-    end
-
-    it "routes to #show" do
-      expect(:get => "/pins/1").to route_to("pins#show", :id => "1")
-    end
-
-    it "routes to #edit" do
-      expect(:get => "/pins/1/edit").to route_to("pins#edit", :id => "1")
-    end
-
-    it "routes to #create" do
-      expect(:post => "/pins").to route_to("pins#create")
-    end
-
-    it "routes to #update via PUT" do
-      expect(:put => "/pins/1").to route_to("pins#update", :id => "1")
-    end
-
-    it "routes to #update via PATCH" do
-      expect(:patch => "/pins/1").to route_to("pins#update", :id => "1")
-    end
-
-    it "routes to #destroy" do
-      expect(:delete => "/pins/1").to route_to("pins#destroy", :id => "1")
-    end
-
+    it { expect(:get => "/pins").to route_to(:controller => "pins", :action => "index") }
+    it { expect(:get => "/pins/1").to route_to(:controller => "pins", :action => "show", :id => "1") }
+    it { expect(:get => "/pins/new").to route_to(:controller => "pins", :action => "new") }
+    it { expect(:post => "/pins").to route_to(:controller => "pins", :action => "create") }
+    it { expect(:get => "/pins/1/edit").to route_to(:controller => "pins", :action => "edit", :id => "1") }
+    it { expect(:patch => "/pins/1").to route_to(:controller => "pins", :action => "update", :id => "1") }
+    it { expect(:delete => "/pins/1").to route_to(:controller => "pins", :action => "destroy", :id => "1") }
   end
 end
