707286b04376421ecf254135890bee345d44d548
JasonZ <jason.zeng@tinkerbox.com.sg>
2015-08-18 15:07:30 +0800

    Implement user comments on pin show with tests

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,42 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_pin
+
+  def new
+    @comment = @pin.comments.build
+  end
+
+  def create
+    @comment = @pin.comments.build(comment_params)
+    @comment.commenter = current_user
+    if @comment.save
+      redirect_to @pin, notice: "Comment created successfully!"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @comment = @pin.comments.find(params[:id])
+  end
+
+  def update
+    @comment = @pin.comments.find(params[:id])
+    if @comment.update_attributes(comment_params)
+      redirect_to @pin, notice: "Comment updated successfully!"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+    def set_pin
+      @pin = Pin.find_by(id: params[:pin_id])
+    end
+
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,15 @@ Rails.application.routes.draw do
   root 'pins#index'
   get '/index' => 'home#index'
   get '/about' => 'home#about'
+
   devise_for :users
+  
   concern :paginatable do
     get '(page/:page)', :action => :index, :on => :collection, :as => ''
   end
-  resources :pins, :concerns => :paginatable
+  
+  resources :pins, :concerns => :paginatable do
+    resources :comments, only: [:new, :create, :edit, :update]
+  end
 
 end
