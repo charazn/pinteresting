ec6a2675e4166cfccf0fdefa96149a3b1efb622b
JasonZ <jason.zeng@tinkerbox.com.sg>
2015-08-21 14:51:03 +0800

    Add > to paperclip has_attached_file styles: { :large }

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -7,7 +7,7 @@ class Pin < ActiveRecord::Base
   validates :description, presence: true
   
   validates :image, :attachment_presence => true
-  has_attached_file :image, :styles => { :large => "600x600", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "missing_:style.png"
+  has_attached_file :image, :styles => { :large => "600x600>", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "missing_:style.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   paginates_per 20
