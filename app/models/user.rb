359041c88d62ffdd28431e0c852c37338c14ccbf
JasonZ <jason.zeng@tinkerbox.com.sg>
2015-08-24 11:16:59 +0800

    Add validation to user model and users controllers with basic password and password_confirmation in form

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,16 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   # devise :database_authenticatable, :registerable,
   #        :recoverable, :rememberable, :trackable, :validatable
+  authenticates_with_sorcery!
 
   has_many :pins
 
   validates :username, presence: true
+  validates :email, uniqueness: true
+
+  validates :password, length: { minimum: 6 }, if: -> { new_record? || changes["password"] }
+  validates :password, confirmation: true, if: -> { new_record? || changes["password"] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes["password"] }
 
   has_many :comments_received, :as => :commentable, :class_name => "Comment"
   has_many :comments_made, :class_name => "Comment", :foreign_key => :commenter_id
