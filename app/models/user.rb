37190a1be91bfd631ebfd7180470fe18ca05d3e6
JasonZ <jason.zeng@tinkerbox.com.sg>
2015-08-25 18:06:50 +0800

    Add image to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,4 +29,7 @@ class User < ActiveRecord::Base
 
   enum status: { archived: 0, active: 1 } 
 
+  has_attached_file :image, :styles => { :medium => "300x300#", :thumb => "100x100#", :profile_navbar => "30x30#" }, :default_url => "missing_:style.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150825042634) do
+ActiveRecord::Schema.define(version: 20150825085636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,10 @@ ActiveRecord::Schema.define(version: 20150825042634) do
     t.string   "username"
     t.string   "salt"
     t.integer  "status",                 default: 1
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
